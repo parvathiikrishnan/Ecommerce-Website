222818b31564ebd72c204964304c41bea0ef459a
parvathiikrishnan <98779654+parvathiikrishnan@users.noreply.github.com>
2024-09-22 10:14:23 +0400

    Added top nav bar and login form UI

--- task3/src/app/app.module.ts
+++ task3/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TopnavComponent,
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
