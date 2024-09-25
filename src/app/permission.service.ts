879285439ea3582bbf2a658ae6fb1350dd487747
parvathiikrishnan <98779654+parvathiikrishnan@users.noreply.github.com>
2024-09-25 12:54:46 +0400

    login in bugfix

--- src/app/permission.service.ts
+++ src/app/permission.service.ts
@@ -4,6 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PermissionService {
-
+  private loggedInUser: { username: string; role: string } | null = null;
   constructor() { }
+
+  Adminlogin(username: string, password: string): boolean {
+    if (username === 'johnd' && password === 'admin123') {
+      this.loggedInUser = { username: 'johnd', role: 'admin' };
+      return true;
+    } 
+    
+    else{
+      this.loggedInUser = { username, role: 'user' };
+      return false;
+    }
+    
+  }
+
+  getUserRole(): string | null {
+    return this.loggedInUser ? this.loggedInUser.role : null;
+  }
 }
